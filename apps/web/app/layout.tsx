2e4f90ab9ea07e038a46f444e738abe6e42b27be
Dao Hoang Son <son@save.day>
2023-07-14 06:57:00 +0700

    Apply `Inter Tight` global font

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,10 @@
+import { Inter_Tight } from "next/font/google";
 import { ApolloWrapper } from "../lib/apollo-wrapper";
 import { StyledComponentsWrapper } from "../lib/styled-components-wrapper";
+
+const interTight = Inter_Tight({
+  subsets: ["latin", "vietnamese"],
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={interTight.className}>
         <ApolloWrapper>
           <StyledComponentsWrapper>{children}</StyledComponentsWrapper>
         </ApolloWrapper>
